01855964e92edcf556cb53d981a0311bd3568292
Deepak Meena <deepmeena70@gmail.com>
2021-09-28 14:00:45 +0530

    expo stripe payments sample

---
+++ config.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://us-central1-agro-fresh-dl-admin-1f174.cloudfunctions.net/api";
--- screens/payments/StripePayment.js
+++ screens/payments/StripePayment.js
@@ -1,7 +1,77 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React,{ useState, useEffect} from 'react'
+import { StyleSheet, Text, View, Button, Alert } from 'react-native'
 import {useSelector} from 'react-redux'
 import {cartDetailsSelector} from '../../features/cartDetails';
+import { StripeProvider, useStripe, clientSecret } from '@stripe/stripe-react-native';
+import {API_URL} from "../../config";
+
+
+function PaymentScreen () {
+    const { initPaymentSheet, presentPaymentSheet } = useStripe();
+    const [loading, setLoading] = useState(false);
+
+    const fetchPaymentSheetParams = async () => {
+        const response = await fetch(`${API_URL}/payment-sheet`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        });
+        const { paymentIntent, ephemeralKey, customer } = await response.json();
+
+        return {
+        paymentIntent,
+        ephemeralKey,
+        customer,
+        };
+    };
+
+    const initializePaymentSheet = async () => {
+        const {
+        paymentIntent,
+        ephemeralKey,
+        customer,
+        } = await fetchPaymentSheetParams();
+
+        const { error, paymentOption } = await initPaymentSheet({
+            customerId: customer,
+            customerEphemeralKeySecret: ephemeralKey,
+            paymentIntentClientSecret: paymentIntent,
+            googlePay: true,
+            merchantCountryCode: 'US',
+            testEnv: true,
+        });
+        if (!error) {
+        setLoading(true);
+        }
+    };
+
+    const openPaymentSheet = async () => {
+
+        const { error } = await presentPaymentSheet({ clientSecret });
+
+        if (error) {
+        Alert.alert(`Error code: ${error.code}`, error.message);
+        } else {
+        Alert.alert('Success', 'Your order is confirmed!');
+        }
+    };
+
+    useEffect(() => {
+        initializePaymentSheet();
+    }, []);
+
+    return (
+        <Button
+            variant="primary"
+            disabled={!loading}
+            title="Checkout"
+            onPress={openPaymentSheet}
+        />
+    )
+
+
+}
 
 export default function StripePayment() {
     const {cartDetails} = useSelector(cartDetailsSelector);
@@ -10,8 +80,12 @@ export default function StripePayment() {
 
     return (
         <View style={styles.container}>
-
             <Text>Pay for {cartDetails.grandTotal}</Text>
+            <StripeProvider
+                publishableKey="pk_test_51IG1hcHEkju5OzqanAHWmpiZjpBXDkUz8eZK1tONEUIi7euisrNWNsPAdwaLXiIZOixtK4MTaK7LvITI3jeipV1700k8EIVuKJ"
+            >
+                <PaymentScreen />
+            </StripeProvider>
         </View>
     )
 }
